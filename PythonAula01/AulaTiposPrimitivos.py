58c233a89da3f6b28c6292389e2e9b465ebaaf11
Diego Bortolucci <bortolucci.diego@gmail.com>
2025-10-28 22:06:21 -0300

    Initial commit

---
+++ PythonAula01/AulaTiposPrimitivos.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite um valor:"))
+n2 = int(input("Digite outro valor:"))
+
+soma = n1 + n2
+print("A soma entre", n1, "e", n2, "é", soma)
+print("A Soma entre {} e {} é {}".format(n1, n2, soma))
---
+++ PythonAula01/AulaTiposPrimitivos2.py
@@ -0,0 +1,4 @@
+n = (input("Digite um numero: "))
+print(n.isnumeric())
+print(n.isalpha())
+print(n.isalnum())
